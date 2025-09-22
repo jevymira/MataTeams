17c8354b4c9ee5eb4bc01c3ac8ce9c6c8ecc9150
Jevy Miranda <jevymira@outlook.com>
2025-09-21 19:50:13 -0700

    feat(identity): user store, login, and jwt generation

---
+++ api/Identity.API/Controllers/ApplicationUserController.cs
@@ -0,0 +1,48 @@
+using Identity.API.Dtos;
+using Identity.API.Model;
+using Identity.API.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApplicationUserController : ControllerBase
+{
+    private readonly ILogger<ApplicationUserController> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly JwtManager _jwtManager;
+
+    public ApplicationUserController(
+        ILogger<ApplicationUserController> logger,
+        UserManager<ApplicationUser> userManager,
+        JwtManager jwtManager)
+    {
+        _logger = logger;
+        _userManager = userManager;
+        _jwtManager = jwtManager;
+    }
+
+    [HttpPost("/login")]
+    public async Task<IActionResult> LoginAsync(string userName, string password)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        
+        if (user == null || !await _userManager.CheckPasswordAsync(user, password))
+        {
+            return Unauthorized("Invalid username or password");
+        }
+
+        var token = await _jwtManager.GetJwtAsync(user);
+
+        var response = new LoginResponse
+        {
+            Success = true,
+            Message = "Login success.",
+            Token = token,
+        };
+
+        return Ok(response);
+    }
+}
---
+++ api/Identity.API/Model/ApplicationUser.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API.Model;
+
+public class ApplicationUser : IdentityUser { }
