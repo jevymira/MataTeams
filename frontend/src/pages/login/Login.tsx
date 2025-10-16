d8d46cbedf17b737282a2c85526575a6e78ba1f3
Alirie Gray <alirie.gray@gmail.com>
2025-10-16 16:18:47 -0700

    Update react versions and add password input

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,12 +1,33 @@
 // libraries 
+import { useState } from 'react'
 import { Link } from 'react-router'
+import { Input, Container, Button } from '@chakra-ui/react'
+import { PasswordInput } from '../../components/ui/password-input'
+
+// hooks
+import { useLogin } from '../../hooks/login'
 
 // style
 import './Login.css'
 
 export const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [login] = useLogin(username, password)
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    login()
+  }
+  
   return (
-  <div className=''>
-    Login
-  </div>
+  <Container maxW='md'>
+    <Input placeholder='Username' size='md' onChange={e => {
+      setUsername(e.target.value)
+    }} />
+    <PasswordInput placeholder='Password' size='md' onChange={e => {
+      setPassword(e.target.value)
+    }}/>
+    <Button onClick={e => handleSubmit(e)}>Log In</Button>
+  </Container>
 )}
