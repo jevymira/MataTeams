20803fc792d634ee63945953a17266c6308455c8
Jevy Miranda <jevymira@outlook.com>
2025-11-11 16:03:04 -0800

    feat(teams): add endpoint to edit authenticated user profile

--- api/src/Teams.API/Extensions/Extensions.cs
+++ api/src/Teams.API/Extensions/Extensions.cs
@@ -194,5 +194,6 @@ internal static class Extensions
         var usersGroup = app.MapGroup("/api/users").WithTags("Users");
         CreateProfile.MapEndpoint(usersGroup);
         GetAuthenticatedUserProfileEndpoint.Map(usersGroup);
+        EditAuthenticatedUserProfileEndpoint.Map(usersGroup);
     }
 }
