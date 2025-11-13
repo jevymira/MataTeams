8a29755fe14ddbdfdc93a24df9f2dac0e868ad0d
Jevy <jevymira@outlook.com>
2025-11-12 16:14:40 -0800

    fix(teams): define delete behavior for team child entities

--- api/src/Teams.Infrastructure/EntityConfigurations/TeamMembershipRequestEntityTypeConfiguration.cs
+++ api/src/Teams.Infrastructure/EntityConfigurations/TeamMembershipRequestEntityTypeConfiguration.cs
@@ -12,13 +12,14 @@ public class TeamMembershipRequestEntityTypeConfiguration : IEntityTypeConfigura
         builder.Property(r => r.Status)
             .HasConversion<string>()
             .IsRequired();
-        
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(r => r.UserId)
+            .OnDelete(DeleteBehavior.NoAction);
+
         builder.HasOne<ProjectRole>()
             .WithMany()
             .HasForeignKey(r => r.ProjectRoleId);
-        
-        builder.HasOne<User>()
-            .WithMany()
-            .HasForeignKey(r => r.UserId);
     }
 }
