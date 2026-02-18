a1dfe0776d4570ef97b725a3d27067b23c94eecc
AlirieGray <alirie.gray@gmail.com>
2026-02-17 21:01:08 -0800

    File upload library

--- frontend/src/pages/signup/signup.tsx
+++ frontend/src/pages/signup/signup.tsx
@@ -1,9 +1,13 @@
 // libraries 
 import { useState } from 'react'
 import { Link } from 'react-router'
-import { Input, Container, Button, Stack, Checkbox, CheckboxCard, CheckboxGroup, Fieldset } from '@chakra-ui/react'
+import { Input, Container, Button, Stack, Checkbox, CheckboxCard, CheckboxGroup, Flex, ScrollArea } from '@chakra-ui/react'
 import { PasswordInput } from '../../components/ui/password-input'
 import SkillsDropdown from '../../components/skillsDropdown/SkillsDropdown'
+import Uppy from '@uppy/core';
+import DragDrop from '@uppy/drag-drop';
+import '@uppy/core/css/style.min.css';
+import '@uppy/drag-drop/css/style.min.css';
 
 // hooks
 import { useSignup} from '../../hooks/signup'
@@ -30,6 +34,11 @@ export const Signup = () => {
         setSkills(formSkills)
     }
 
+    const setSignupFormSkillsFromPlaceholderResume = () => {
+        const placeholderSkills: Skill[] = []
+        setSkills(placeholderSkills)
+    }
+
     const [signup] = useSignup(email, password, firstName, lastName, username, isFaculty, skills)
 
     const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -37,61 +46,71 @@ export const Signup = () => {
         signup()
     }
 
+    new Uppy().use(DragDrop, { target: '#drag-drop' });
+
     return (
-        <div className='background'>
-            <Container maxW='2xl' className='Form'>
-                <p>Welcome!</p>
-                <p>Let's get you set up!</p>
+        <Flex alignItems={'center'} flexDirection={'column'}>
+            <ScrollArea.Root maxW='2xl' className='Form' height="80vh" >
+                <ScrollArea.Viewport>
+                    <ScrollArea.Content padding={'15px'}>
+                        <p>Welcome!</p>
+                        <p>Let's get you set up!</p>
 
-                <p>Please enter your first and last name:</p>
-                <Input placeholder='First Name' size='md' onChange={e => {
-                setFirst(e.target.value)
-                }}/>
+                        <p>Please enter your first and last name:</p>
+                        <Input placeholder='First Name' size='md' onChange={e => {
+                        setFirst(e.target.value)
+                        }}/>
 
-                <Input placeholder='Last Name' size='md' onChange={e => {
-                setLast(e.target.value)
-                }}/>
+                        <Input placeholder='Last Name' size='md' onChange={e => {
+                        setLast(e.target.value)
+                        }}/>
 
-                <p>Please enter your CSUN email:</p>
-                <Input placeholder='Email' size='md' onChange={e => {
-                setEmail(e.target.value)
-                }}/>
+                        <p>Please enter your CSUN email:</p>
+                        <Input placeholder='Email' size='md' onChange={e => {
+                        setEmail(e.target.value)
+                        }}/>
 
-                <p>Create a Username and Password</p>
-                <Input placeholder='Username' size='md' onChange={e => {
-                setUsername(e.target.value)
-                }}/>
+                        <p>Create a Username and Password</p>
+                        <Input placeholder='Username' size='md' onChange={e => {
+                        setUsername(e.target.value)
+                        }}/>
 
-                <Input placeholder='Password' type='password' size='md' onChange={e => {
-                setPassword(e.target.value)
-                }}/>
+                        <Input placeholder='Password' type='password' size='md' onChange={e => {
+                        setPassword(e.target.value)
+                        }}/>
 
-                <p>Are you a Student or Faculty member?</p>
-                    <CheckboxCard.Root variant='outline' colorPalette='green'
-                            checked={isFaculty ===true} onCheckedChange={() => setFaculty(true)}>
-                        <CheckboxCard.HiddenInput />
-                        <CheckboxCard.Control>
-                            <CheckboxCard.Label>Faculty</CheckboxCard.Label>
-                            <CheckboxCard.Indicator />
-                        </CheckboxCard.Control>
-                    </CheckboxCard.Root>
+                        <p>Are you a Student or Faculty member?</p>
+                            <CheckboxCard.Root variant='outline' colorPalette='green'
+                                    checked={isFaculty ===true} onCheckedChange={() => setFaculty(true)}>
+                                <CheckboxCard.HiddenInput />
+                                <CheckboxCard.Control>
+                                    <CheckboxCard.Label>Faculty</CheckboxCard.Label>
+                                    <CheckboxCard.Indicator />
+                                </CheckboxCard.Control>
+                            </CheckboxCard.Root>
 
-                    <CheckboxCard.Root variant='outline' colorPalette='green'
-                            checked={isFaculty ===false} onCheckedChange={() => setFaculty(false)}>
-                        <CheckboxCard.HiddenInput />
-                        <CheckboxCard.Control>
-                            <CheckboxCard.Label>Student</CheckboxCard.Label>
-                            <CheckboxCard.Indicator />
-                        </CheckboxCard.Control>
-                    </CheckboxCard.Root>
+                            <CheckboxCard.Root variant='outline' colorPalette='green'
+                                    checked={isFaculty ===false} onCheckedChange={() => setFaculty(false)}>
+                                <CheckboxCard.HiddenInput />
+                                <CheckboxCard.Control>
+                                    <CheckboxCard.Label>Student</CheckboxCard.Label>
+                                    <CheckboxCard.Indicator />
+                                </CheckboxCard.Control>
+                            </CheckboxCard.Root>
 
-                <p>Enter your major:</p>
-                <Input placeholder='Major' size='md'/>
+                        <p>Enter your major:</p>
+                        <Input placeholder='Major' size='md'/>
 
-                <SkillsDropdown labelText='Select your skills' setFormSkills={handleSetSignupFormSkills}/>
-                <Button onClick={e => handleSubmit(e)}>Submit</Button>
-                <br /><p>Already have an account? <Link to='/login'>Login here!</Link></p>
-            </Container>
-        </div>
+                        <SkillsDropdown labelText='Select your skills' setFormSkills={handleSetSignupFormSkills}/>
+                        <Button onClick={e => handleSubmit(e)}>Submit</Button>
+                        <br /><p>Already have an account? <Link to='/login'>Login here!</Link></p>
+                    </ScrollArea.Content>
+                </ScrollArea.Viewport>
+                <ScrollArea.Scrollbar height="70vh">
+                <ScrollArea.Thumb />
+                </ScrollArea.Scrollbar>
+                <ScrollArea.Corner />
+            </ScrollArea.Root>
+        </Flex>
     )
   }
